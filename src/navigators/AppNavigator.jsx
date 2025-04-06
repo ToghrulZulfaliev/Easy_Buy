1e6ec699784c7cde22f293773bc1de5fa1e88d7b
ToghrulZulfaliev <toghrulzulfaliev8@gmail.com>
2025-04-06 13:25:29 +0400

    Fix import path for HomePage

--- src/navigators/AppNavigator.jsx
+++ src/navigators/AppNavigator.jsx
@@ -2,7 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../components/Layout/Layout';
-import HomePage from '../pages/HomePage';
+import HomePage from "../pages/HomePage";
+
+
 import ProductsPage from '../pages/ProductsPage';
 import AboutPage from '../pages/AboutPage';
 import ContactPage from '../pages/ContactPage';
